141311a7943749bb5194baafd10d5522b8476cb8
Imtraped <91901629+Imtraped@users.noreply.github.com>
2023-06-12 11:35:45 -0400

    Renamed Folder Names
    The Cmd prompt won't cd into them with the default names.

---
+++ exercises/Etch-a-Sketch/etch-a-sketch.js
@@ -0,0 +1,14 @@
+// Select the elements on the page - canvas, shake button
+let canvas = document.querySelector('#etch-a-sketch');
+
+let ctx = canvas.getContext('2d');
+
+// Setup our canvas for drawing.
+
+// Write a Draw function
+
+// Write a handler for the keys
+
+// Clear/Shake Function
+
+// Listen for arrow keys
