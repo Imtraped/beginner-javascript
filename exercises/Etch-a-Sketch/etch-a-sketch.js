32b8aa608821d1e83523ab26ba7e588524a5d73a
Imtraped <91901629+Imtraped@users.noreply.github.com>
2023-06-12 11:57:57 -0400

    Randomness
    The Dot will now appear in random places.

--- exercises/Etch-a-Sketch/etch-a-sketch.js
+++ exercises/Etch-a-Sketch/etch-a-sketch.js
@@ -3,7 +3,23 @@ let canvas = document.querySelector('#etch-a-sketch');
 
 let ctx = canvas.getContext('2d');
 
+let shakebutton = document.querySelector('.shake');
 // Setup our canvas for drawing.
+// Same Names with the variables and properties.
+const {width, height} = canvas;
+
+// Create random x and y starting points on the canvas
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * width);
+
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 10;
+
+ctx.beginPath(); // Start the Drawing
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
+ctx.stroke();
 
 // Write a Draw function
 
