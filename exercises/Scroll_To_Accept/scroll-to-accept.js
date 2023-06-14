c9439cb24bf44b322f267a18976350d8197907c9
Imtraped <91901629+Imtraped@users.noreply.github.com>
2023-06-14 13:40:35 -0400

    Update scroll-to-accept.js

---
+++ exercises/Scroll_To_Accept/scroll-to-accept.js
@@ -0,0 +1,10 @@
+const terms = document.querySelector('.terms-and-conditions');
+const watch = document.querySelector('.watch');
+
+function obCallback(payload) {
+    console.log(payload[0].intersectionRatio);
+}
+
+const ob = new IntersectionObserver(obCallback);
+
+ob.observe(terms.lastElementChild);
