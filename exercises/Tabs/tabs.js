aa9e69604d5d17f02300789ea790222c565f8240
Imtraped <91901629+Imtraped@users.noreply.github.com>
2023-06-16 10:55:24 -0400

    Tabs!!!!!!11111
    The Tabs are in working order.

--- exercises/Tabs/tabs.js
+++ exercises/Tabs/tabs.js
@@ -1,9 +1,29 @@
 const tabs = document.querySelector('.tabs');
 const tabButtons = tabs.querySelectorAll('[role="tab"]');
-const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
 
 function handleTabClick(event) {
-    console.log(event);
+    // Hide all tab panels
+    tabPanels.forEach(panel => {
+        panel.hidden = true;
+    });
+    // Mark all tabs as unselected
+    tabButtons.forEach(tab => {
+        // tab.ariaSelected = false;
+        tab.setAttribute('aria-selected', false);
+    });
+    // Mark the clicked tab as selected
+    event.currentTarget.setAttribute('aria-selected', true);
+    // Find the associated tabPanel and show it!
+    const { id } = event.currentTarget;
+
+    // METHOD 1
+    /*const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+    tabPanel.hidden = false;*/
+
+    // METHOD 2
+    const tabPanel = tabPanels.find(panel => panel.getAttribute('aria-labelledby') === id);
+    tabPanel.hidden = false;
 }
 
 tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
