44e0356e6aa6a169e8b3436d4a79939459f00db5
Imtraped <91901629+Imtraped@users.noreply.github.com>
2023-10-19 11:38:15 -0400

    Update shopping.js
    It's done!

--- exercises/Shopping_List/shopping.js
+++ exercises/Shopping_List/shopping.js
@@ -2,7 +2,7 @@ const shoppingForm = document.querySelector('.shopping');
 const list = document.querySelector('.list');
 
 // Array to hold our state
-const items = [];
+let items = [];
 
 function handleSubmit(e) {
     e.preventDefault();
@@ -25,9 +25,9 @@ function handleSubmit(e) {
 
 function displayItems() {
     const html = items.map(item => `<li class="shopping-item">
-    <input type="checkbox">
+    <input value="${item.id}" type="checkbox" ${item.complete ? 'checked' : ''}>
     <span class="itemName">${item.name}</span>
-    <button aria-label="Remove ${item.name}">&times;</button>
+    <button aria-label="Remove ${item.name}" value="${item.id}">&times;</button>
     </li>`)
     .join('');
     list.innerHTML = html;
@@ -48,8 +48,31 @@ function restoreFromLocalStorage() {
     }
 }
 
+function deleteItem(id) {
+    console.log(`The item ${id} has been deleted.`);
+    // Update our items array without this one.
+    items = items.filter(item => item.id !== id);
+    list.dispatchEvent(new CustomEvent('itemsUpdated'));
+}
+
+function markAsComplete(id) {
+    const itemRef = items.find(item => item.id === id);
+    itemRef.complete = !itemRef.complete;
+    list.dispatchEvent(new CustomEvent('itemsUpdated'));
+}
+
 shoppingForm.addEventListener('submit', handleSubmit);
 list.addEventListener('itemsUpdated', displayItems);
 list.addEventListener('itemsUpdated', mirrorToLocalStorage);
+// Event Delegation: Listen on the click for the list but then delegate that click to the button if it was clicked.
+list.addEventListener('click', function(e) {
+    const id = parseInt(e.target.value);
+    if(e.target.matches('button')) {
+        deleteItem(id);
+    }
+    if (e.target.matches('input[type="checkbox"]')) {
+        markAsComplete(id);
+    }
+});
 
 restoreFromLocalStorage();
