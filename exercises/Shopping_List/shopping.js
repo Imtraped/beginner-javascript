f199556d61d5d6ec385138b0088e055bb3bb1f96
Imtraped <91901629+Imtraped@users.noreply.github.com>
2023-10-19 08:09:26 -0400

    Update shopping.js
    We're getting into custom events, it's getting real.

--- exercises/Shopping_List/shopping.js
+++ exercises/Shopping_List/shopping.js
@@ -7,9 +7,31 @@ const items = [];
 function handleSubmit(e) {
     e.preventDefault();
     console.log('submitted the item, too bad it\'s useless.');
-    console.log(e.currentTarget);
-    const name = e.currentTarget.value;
-    console.log(name);
+    console.log(e.currentTarget.item);
+    const name = e.currentTarget.item.value;
+    // If it's empty, then don't submit it.
+    if(!name) return;
+    const item = {
+        name: name,
+        id: Date.now(),
+        complete: false,
+    };
+    // Push the items into our state
+    items.push(item);
+    console.log(`There are now ${items.length} in your state`);
+    // Clear the form
+    e.target.reset();
+    displayItems();
 };
+
+function displayItems() {
+    const html = items.map(item => `<li class="shopping-item">
+    <input type="checkbox">
+    <span class="itemName">${item.name}</span>
+    <button aria-label="Remove ${item.name}">&times;</button>
+    </li>`)
+    .join('');
+    list.innerHTML = html;
+}
 
 shoppingForm.addEventListener('submit', handleSubmit);
